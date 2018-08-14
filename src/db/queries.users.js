1efc31b1186d74762f027d2fc0bb5c3830f8d2bd
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2018-08-14 08:39:48 -0700

    sendgrid added

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,22 +1,30 @@
 const User = require("./models").User;
 const bcrypt = require("bcryptjs");
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = {
   createUser(newUser, callback){
-
     const salt = bcrypt.genSaltSync();
     const hashedPassword = bcrypt.hashSync(newUser.password, salt);
-
     return User.create({
+      username: newUser.username,
       email: newUser.email,
       password: hashedPassword
     })
     .then((user) => {
+      const msg = {
+        to: newUser.email,
+        from: 'donotreply@example.com',
+        subject: 'Bloom Account Confirmation',
+        text: 'Welcome to Bloom',
+        html: '<strong>Please login to start shopping!</strong>',
+      };
+      sgMail.send(msg);
       callback(null, user);
     })
     .catch((err) => {
       callback(err);
     })
   }
-
 }
