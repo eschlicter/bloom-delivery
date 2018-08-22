58720c9403a0a6b5cecb3f20be055ff1d5ea541d
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2018-08-22 14:47:28 -0700

    pictures for products implemented

--- src/db/models/product.js
+++ src/db/models/product.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   var Product = sequelize.define('Product', {
     title: DataTypes.STRING,
     description: DataTypes.STRING,
-    price: DataTypes.INTEGER
+    price: DataTypes.INTEGER,
+    image: DataTypes.STRING
   }, {});
   Product.associate = function(models) {
     // associations can be defined here
