36ff5c79b896839d669744c04ecc6ae1747acfb3
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2018-09-07 11:36:05 -0700

    deploy

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
-const logger = require('morgan');
 const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
 const session = require("express-session");
 const flash = require("express-flash");
 const passportConfig = require("./passport-config");
-const Sequelize = require('sequelize')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const Sequelize = require('sequelize')
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const logger = require('morgan');
 
-var sequelize = new Sequelize(
-    "bloom-dev",
-    "postgres",
-    "password", {
-        "dialect": "postgres"
-    });
+// var sequelize = new Sequelize(
+//     "bloom-dev",
+//     "postgres",
+//     "password", {
+//         "dialect": "postgres"
+//     });
 
 module.exports = {
     init(app, express){
@@ -27,14 +27,14 @@ module.exports = {
         
         app.use(expressValidator());
         app.use(session({
-            store: new SequelizeStore({
-                db: sequelize
-            }),
+            // store: new SequelizeStore({
+            //     db: sequelize
+            // }),
             secret: process.env.cookieSecret,
             resave: false,
             saveUninitialized: false,
             cookie: { maxAge: 1.21e+9 }, //set cookie to expire in 14 days
-            proxy: true
+            // proxy: true
         }));
         
         app.use(flash());
